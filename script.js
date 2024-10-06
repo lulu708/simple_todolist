6db04d875b800c7a04b8e69d5f176ab3924dbc63
lulu708 <lulugogo7993@gmail.com>
2024-10-06 21:22:45 +0900

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function deleteItem(element) {
-  const todoItem = element.parentElement;
-  todoItem.remove();
-}
+document.querySelector('.add-button').addEventListener('click', function() {
+  const inputField = document.querySelector('.input-field');
+  const todoText = inputField.value.trim();
+
+  if (todoText) {
+    const newTodoItem = document.createElement('div');
+    newTodoItem.className = 'todo-item';
+    newTodoItem.innerHTML = `
+      <div class="checkbox"></div>
+      <button class="delete-button" onclick="deleteItem(this)">×</button>
+    `;
+
+    document.querySelector('.todo-list').appendChild(newTodoItem); // 동적으로 추가
+    inputField.value = ''; // 입력 필드 비우기
+  }
+});
